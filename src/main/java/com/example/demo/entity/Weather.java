61f1a05258eced6f58ae22d74ca5de7fcff2f864
lytyazla <48278370+lytyazla@users.noreply.github.com>
2020-09-10 16:46:57 +0900

    11

---
+++ src/main/java/com/example/demo/entity/Weather.java
@@ -0,0 +1,81 @@
+package com.example.demo.entity;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "weather")
+public class Weather {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "location_id")
+	private Integer locationId;
+
+	private String name;
+
+	private Integer temperature;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(Integer temperature) {
+		this.temperature = temperature;
+	}
+
+	public Integer getHumidity() {
+		return humidity;
+	}
+
+	public void setHumidity(Integer humidity) {
+		this.humidity = humidity;
+	}
+
+	public Timestamp getDate_time() {
+		return date_time;
+	}
+
+	public void setDate_time(Timestamp date_time) {
+		this.date_time = date_time;
+	}
+
+	private Integer humidity;
+
+	private Timestamp date_time;
+
+	public Integer getLocationId() {
+		return locationId;
+	}
+
+	public void setLocationId(Integer locationId) {
+		this.locationId = locationId;
+	}
+
+
+}
